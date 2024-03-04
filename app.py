c865da4a6c15f24a008e14dbec7e814a2185cb0b
=Mano <=Mano@xyz.com>
2024-03-04 13:26:55 -0600

    "new commit 1"

--- app.py
+++ app.py
@@ -2,23 +2,6 @@ from flask import Flask, jsonify
 import boto3
 
 app = Flask(__name__)
-
-@app.route('/ask_me', methods = ['POST'])
-def get_json_data():
-    data = {"message" : "Hello, what do you want?"}
-    return jsonify(data)
-
-@app.route('/ask_me_anything')
-def get_data():
-    data = {"message" : "Hello, what do you want?"}
-    with open('test.txt','w') as f:
-        f.write(data)
-
-    return jsonify(data)
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0",debug=True)
-
 def upload_to_S3(bucket_name, file_path, object_name):
     aws_access_key_id = ''
     aws_secret_access_key = ''
@@ -37,11 +20,23 @@ def upload_to_S3(bucket_name, file_path, object_name):
         return False
 
     return True
+@app.route('/ask_me', methods = ['POST'])
+def get_json_data():
+    data = {"message" : "Hello, what do you want?"}
+    return jsonify(data)
+
+@app.route('/ask_me_anything')
+def get_data():
+    data = {"message" : "Hello, what do you want?"}
+    with open('test.txt','w') as f:
+        f.write(data)
+
+    upload_to_S3('mynewbucket-for-myapp', 'test.txt', 'test.txt')
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",debug=True)
 
 
-if __name__ == "__main__":
-    bucket_name = 'mynewbucket-for-myapp'
-    file_path = 'requirements.txt'
-    object_name = 'requirements.txt'
 
-    upload_to_S3(bucket_name, file_path, object_name)
+
