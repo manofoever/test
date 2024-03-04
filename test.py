192b1a677d1b137cf1275542cd659080a44f97f0
=Mano <=Mano@xyz.com>
2024-03-04 13:11:35 -0600

    "new commit 1"

--- test.py
+++ test.py
@@ -1,23 +1,5 @@
-from flask import Flask, jsonify
 import boto3
 
-app = Flask(__name__)
-
-@app.route('/ask_me', methods = ['POST'])
-def get_json_data():
-    data = {"message" : "Hello, what do you want?"}
-    return jsonify(data)
-
-@app.route('/ask_me_anything')
-def get_data():
-    data = {"message" : "Hello, what do you want?"}
-    with open('test.txt','w') as f:
-        f.write(data)
-
-    return jsonify(data)
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0",debug=True)
 
 def upload_to_S3(bucket_name, file_path, object_name):
     aws_access_key_id = ''
