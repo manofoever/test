425ce687ee4a905e544f71f6d8f0c519e9c38486
xyz <xyz@zyz.com>
2024-03-04 20:05:13 +0000

    new_commit

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, jsonify
 import boto3
 
+
 app = Flask(__name__)
+
+count = 0
+
 def upload_to_S3(bucket_name, file_path, object_name):
-    aws_access_key_id = ''
-    aws_secret_access_key = ''
+    aws_access_key_id = 'AKIA2QF4WYZBICZ5CN66'
+    aws_secret_access_key = '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'
     aws_region = 'us-east-1'
     s3 = boto3.client('s3',
                       aws_access_key_id=aws_access_key_id,
@@ -27,12 +31,15 @@ def get_json_data():
 
 @app.route('/ask_me_anything')
 def get_data():
+    global count
     data = {"message" : "Hello, what do you want?"}
-    with open('test.txt','w') as f:
+    with open(f'test_{count}.txt','w') as f:
         f.write(str(data))
+        count+=1
 
-    upload_to_S3('mynewbucket-for-myapp', 'test.txt', 'test.txt')
+    upload_to_S3('mynewbucket-for-myapp', 'test.txt', f'input/test_{count}.txt')
     return jsonify(data)
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",debug=True)
