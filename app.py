a594d21dafd905b6adc4423b3e8c4e17aa039ba2
Manoj <51278481+manofoever@users.noreply.github.com>
2024-03-04 12:17:21 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ def get_json_data():
     data = {"message" : "Hello, what do you want?"}
     return jsonify(data)
 
+@app.route('/ask_me_anything')
+def get_data():
+    data = {"message" : "Hello, what do you want?"}
+    return jsonify(data)
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0",debug=True)
